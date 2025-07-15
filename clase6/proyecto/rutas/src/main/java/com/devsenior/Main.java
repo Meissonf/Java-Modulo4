c4b851eb507c4ce4e7e155d40e75b4c5c262bee7
Alfonso Lara <orozcoenrique159@gmail.com>
2025-07-14 21:00:01 -0500

    se prueba la aplicacion hasta clases creadas

--- clase6/proyecto/rutas/src/main/java/com/devsenior/Main.java
+++ clase6/proyecto/rutas/src/main/java/com/devsenior/Main.java
@@ -2,6 +2,13 @@ package com.devsenior;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RutaTuristica ruta = new RutaTuristica("Ruta Cafetera", "Recorrido por fincas cafeteras", "Eje cafetero");
+
+        ruta.agregarEtiqueta("café");
+        ruta.agregarEtiqueta("naturaleza");
+        ruta.agregarEtiqueta("cultura");
+
+        ruta.agregarPunto(new PuntoInteres("Finca El atardecer", "Tour por eje cultural y productivo", "5.005, -75.343"));
+        System.out.println(ruta);
     }
 }
